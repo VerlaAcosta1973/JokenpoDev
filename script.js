6300b8a3cb3da091796568fc43842cb2624f1bb0
verlaAcostaRamos <eskurinhoes082023@gmail.com>
2024-09-16 07:40:20 -0300

    Adicionado o conceito de ENUM

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ const humanScore = document.querySelector('#human-score')
 const machineScore = document.querySelector('#machine-score')
 const figureResult = document.querySelector('.figure-result')
 
+const GAME_OPTIONS = {
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors'
+}
+
 const playHuman = (humanChoice) => {
     playTheGame(humanChoice, playMachine())
 }
 
 const playMachine = () => {
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
     const randomNumber = Math.floor(Math.random() * 3)
 
     return choices[randomNumber]
@@ -24,7 +30,7 @@ const playTheGame = (human, machine) => {
     if(human === machine){
         result.innerHTML = "Deu empate!!!"
         figureResult.innerHTML = "&#x1F4AA";
-    }else if((human === 'paper' && machine === 'rock') || (human === 'rock' && machine === 'scissors') || (human === 'scissors' && machine === 'paper')){
+    }else if((human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK) || (human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS) || (human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER)){
         human_score++
         humanScore.innerHTML = human_score
         result.innerHTML = "Você Ganhou!!!"
